bd1e11d978b7ab498198763eef5edb38b1c38991
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 12:46:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+
+end
+
+def swap_elements(array)
+
+end
